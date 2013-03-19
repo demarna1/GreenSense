31eab0042926281e10ffc18df9783f2f231f64e9
Noah <noah.demarco@gmail.com>
2013-03-19 16:58:19 -0400

    Successful upload of data via web service

--- PiController/upload.py
+++ PiController/upload.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python
 
 import sys
+import urllib
+import urllib2
 
-if len(sys.argv) == 2:
-	# Parse data; delimiter is ','
-	data = sys.argv[1].split(',')
-	if len(data) == 3:
-		print "P: Temp =", data[0]
-		print "P: Humid =", data[1]
-		print "P: Soil =", data[2]
-	else:
-		print "P: Invalid data."
-else:
-	print "P: Improper arguments supplied."
+# Check number of arguments
+if len(sys.argv) != 2:
+	"P: Improper arguments supplied."
+	sys.exit()
+
+# Parse data; delimiter is ','
+data = sys.argv[1].split(',')
+if len(data) != 3:
+	print "P: Invalid data."
+	sys.exit()
+
+# Encode URL POST parameters and set up the request
+url = "http://wwwtest.cse.lehigh.edu/~nad213/service.cgi"
+post_data = {'temp':data[0], 'humid':data[1], 'soil':data[2]}
+encoded_post_data = urllib.urlencode(post_data)
+request_object = urllib2.Request(url, encoded_post_data)
+
+# Try to access server. Handle exception if unsuccessful
+try:
+	response = urllib2.urlopen(request_object)
+except urllib2.URLError, e:
+	# Failed to connect to server
+	print "P: Upload failed:", e.reason
+	sys.exit()
+	
+# Read data from response into string
+html_string = response.read()
+print "P:", html_string
 
