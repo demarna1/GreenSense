7c7703fc35395b3897cc10199f8295dd1facb42e
demarna1 <noah.demarco@gmail.com>
2013-04-16 18:23:26 +0000

    Updated sensor equations. Extra control to peripherals.

--- PiController/pi_receiver.c
+++ PiController/pi_receiver.c
@@ -16,9 +16,19 @@
 #define HEAD 0x4B
 #define DATA_SIZE 7
 
-// Execute a bash script and pass data to it. Return -1 if unsuccessful, 
+void adjust_controls(double temp, double humid, double soil) {
+	if (humid > 40) {
+		digitalWrite(GPIO_PIN_17, ON);
+		digitalWrite(GPIO_PIN_18, ON);
+	} else {
+		digitalWrite(GPIO_PIN_17, OFF);
+		digitalWrite(GPIO_PIN_18, OFF);
+	}
+}
+
+// Execute a bash script and pass data to it. Return -1 if unsuccessful,
 // and 0 if successful.
-int bash_upload(unsigned int temp, unsigned int humid, unsigned int soil) {
+int bash_upload(double temp, double humid, double soil) {
 	// Declare variables
 	int status;
 	pid_t pid;
@@ -26,7 +36,7 @@ int bash_upload(unsigned int temp, unsigned int humid, unsigned int soil) {
 	char *script = "./upload.sh";
 
 	// Create argument array to be passed to bash
-	sprintf(data_arg, "%d,%d,%d", temp, humid, soil);
+	sprintf(data_arg, "%f,%f,%f", temp, humid, soil);
 
 	// Fork a new process which will run bash script
 	pid = fork();
@@ -58,11 +68,28 @@ int bash_upload(unsigned int temp, unsigned int humid, unsigned int soil) {
 	return 0;
 }
 
+double convert_temp(unsigned int temp_raw) {
+	double volt = temp_raw/1024.0;
+	return volt * 4.5;
+}
+
+double convert_humid(unsigned int humid_raw) {
+	int freq = 15000000/humid_raw;
+	double humid = (993621.0/freq - 163)/0.35;
+	return humid;
+}
+
+double convert_soil(unsigned int soil_raw) {
+	double soil = soil_raw/1024.0;
+	return soil * 4.5;
+}
+
 void read_remaining_bytes(int fd, unsigned char *old_buffer, int len, int index) {
 	// Declare variables
 	unsigned char new_buffer[DATA_SIZE];
 	unsigned char data[DATA_SIZE];
-	unsigned int temp, humid, soil;
+	unsigned int temp_raw, humid_raw, soil_raw;
+	double temp, humid, soil;
 	unsigned char checksum, chk;
 	int i, new_len, count = 0;
 
@@ -83,41 +110,42 @@ void read_remaining_bytes(int fd, unsigned char *old_buffer, int len, int index)
 		}
 	}
 
-	// Data received, turn on LEDs
-	//digitalWrite(GPIO_PIN_17, ON);
-	//digitalWrite(GPIO_PIN_18, ON);
-
 	// All data has been received, place into variables
-	temp = data[0] << 8;
-	temp |= data[1];
-	humid = data[2] << 8;
-	humid |= data[3];
-	soil = data[4] << 8;
-	soil |= data[5];
+	temp_raw = data[0] << 8;
+	temp_raw |= data[1];
+	humid_raw = data[2] << 8;
+	humid_raw |= data[3];
+	soil_raw = data[4] << 8;
+	soil_raw |= data[5];
 	checksum = data[6];
 
 	// Evaluate the checksum
-	chk = temp + humid + soil;
+	chk = temp_raw + humid_raw + soil_raw;
 	if (chk == checksum) {
+		// Convert data
+		temp = convert_temp(temp_raw);
+		humid = convert_humid(humid_raw);
+		soil = convert_soil(soil_raw);
+
 		// Print data
-		printf("temp = %d\n", temp);
-		printf("humid = %d\n", humid);
-		printf("soil = %d\n", soil);
+		printf("temp = %f\n", temp);
+		printf("humid = %f\n", humid);
+		printf("soil = %f\n", soil);
+
 		// Upload data via bash script
 		if (bash_upload(temp, humid, soil) == 0) {
-			printf("Data sent to bash script.\n\n");
+			printf("Data sent to bash script.\n");
 		}
 		else {
-			printf("Data failed to upload.\n\n");
+			printf("Data failed to upload.\n");
 		}
+
+		// Turn on/off fans, etc
+		adjust_controls(temp, humid, soil);
 	}
 	else {
 		printf("Bad checksum\n");
 	}
-
-	// Done handling data, turn off LEDs
-	//digitalWrite(GPIO_PIN_17, OFF);
-	//digitalWrite(GPIO_PIN_18, OFF);
 }
 
 int main() {
@@ -126,14 +154,14 @@ int main() {
 	int i, rx_length;
 	unsigned char rx_buffer[256];
 
-	/*if (wiringPiSetup() == -1) {
+	if (wiringPiSetup() == -1) {
 		fprintf(stderr, "Error setting up wiring Pi library.\n");
 		exit(1);
-	}*/
+	}
 
 	// Set pins as output
-	//pinMode(0, OUTPUT);
-	//pinMode(1, OUTPUT);
+	pinMode(0, OUTPUT);
+	pinMode(1, OUTPUT);
 
 	// Open UART filestream
 	uart0_filestream = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY);
